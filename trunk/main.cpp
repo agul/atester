ccb4f8367ebc402e09afd1d332495126d9d8b649
alexander.agulenko@gmail.com <alexander.agulenko@gmail.com@710d3f46-fe63-0304-d8a4-eb4b2c699f63>
2012-07-25 17:08:08 +0000

    some new modifications

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,26 +1,24 @@
 #include "common.h"
 #include "invocation.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char ** argv) {
 	cin.sync_with_stdio(0);
 	srand((unsigned int)time(NULL) + rand() * 239);
 	
 	showStartUpInfo();
 	initializeFlags();
-	Invocation * Attempt = new Invocation();
-	cout << endl;
-	Attempt->transformParams(argc, argv);
+
+	Invocation * Attempt = new Invocation(argc, argv);
 	Attempt->getCFGFileName();
 	Attempt->getNoWarnings();
-	cleanWarningsQueue();
 	Attempt->loadCFGFile();
 	Attempt->loadParams();
-	//Attempt->showHelp();
-
-
-	/* printf("%d\n", argc);
-	for (int i = 0; i < argc; ++i)
-		printf("%d : %s\n", i, argv[i]); */
+	Attempt->showHelp();
+	cleanWarningsQueue();
+	//Attempt->createEnvironment();
+	//Attempt->runTesting();
+	//Attempt->clearEnvironment();
+	//Attempt->outputInfo();
 
 	system("pause");
 	return 0;
