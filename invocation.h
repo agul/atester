970df55d99e6610618e32bc1b0d3764b917aa75d
alexander.agulenko@gmail.com <alexander.agulenko@gmail.com@710d3f46-fe63-0304-d8a4-eb4b2c699f63>
2012-07-26 13:07:09 +0000

    Parameters.h & Information.h added - structure modifications AguL_STD added AguL_WinAPI added but not developed yet

--- invocation.h
+++ invocation.h
@@ -1,75 +1,7 @@
 #pragma once
 
-struct parameters {
-	bool help, helpCheckers, helpConfig, helpDefault, helpMasks;
-	int ml, tc, tl;
-	string c, CFGfile, i, o, p;
-
-	struct testFileMask {
-		string beginPart, endPart, full;
-		int digits;
-		bool numberNeeded;
-
-		void parse() {
-			beginPart = "";
-			endPart = "";
-			digits = 0;
-			numberNeeded = true;
-			int ls = full.length(), x = 0, y;
-			while (x < ls && full[x] != '?') ++x;
-			if (x == ls) {
-				beginPart = full;
-				numberNeeded = false;
-				return;
-			}
-			beginPart = full.substr(0, x);
-			y = x;
-			while (x < ls && full[x] == '?') ++x;
-			digits = x - y;
-			endPart = full.substr(x);
-			size_t includesNum = endPart.find("?");
-			if (includesNum >= 0 && includesNum < endPart.length()) generateError("test file mask can include only one block differentiating test number");
-		}
-
-	} tim, tom;
-
-	parameters() {
-		c = "std::fcmp";
-		help = false;
-		helpCheckers = false;
-		helpMasks = false;
-		helpDefault = false;
-		helpConfig = false;
-		i = "input.txt";
-		CFGfile = "atester.cfg";
-		ml = 256000000;
-		o = "output.txt";
-		p = "task.exe";
-		tc = 0;
-		tim.full = "??";
-		tl = 2000;
-		tom.full = "??.a";
-	}
-
-};
-
-struct information {
-	int timePeak, memoryPeak;
-	OutcomeType outcome;
-	string iid, code;
-	bool CFGFileSet;
-	char invocationDirectory[1024], checkerPath[1024], workingDirectory[1024];
-
-	information() {
-		iid = toa((int)GetCurrentProcessId());
-		code = "ATester Invocation " + iid;
-		memoryPeak = 0;
-		timePeak = 0;
-		outcome = OT_UD;
-		CFGFileSet = false;
-	}
-
-};
+#include "Parameters.h"
+#include "Information.h"
 
 class Invocation {
 public:
@@ -87,7 +19,7 @@ public:
 	void error(string msg);
 	void showHelp();
 private:
-	parameters params;
-	information info;
+	Parameters * params;
+	Information * info;
 	vector<string> cmdParams;
 };
